3f76fc181b90ec07ffacceedfbc07a1c76bbafa1
WeiChien Hsu <weichien711@gmail.com>
2017-10-14 21:17:09 -0500

    update css

--- test.js
+++ test.js
@@ -1,24 +1,36 @@
 function solve(grades){
     if (grades < 38){
         return grades;
-    } while(grades%10 != 0){
+    } else{
+    
+    // while(grades%10 != 0){
         if(grades%10 > 5){ //66,67,68,69
-            if(grades%5 > 2){ //68,69
-                grades ++;
-            } else{ //66 67
-                grades --;
-            } 
+            if(grades%5 == 2){ //67
+                grades -= 2;
+            }else if(grades%5 == 1){ //66
+            	grades -= 1;
+            }else if(grades%5 == 3){ //68
+                grades += 2;
+            } else{
+            	grades += 1;
+            }
         } // >5
         else if(grades % 10 < 5){ //61,62,63,64
-            if(grades%5 > 2){
-                grades ++;
+            if(grades%5 == 2){ //62
+                grades -= 2;
+            }else if(grades%5 == 1){ //61
+            	grades -= 1;
+            }else if(grades%5 == 3){ //63
+                grades += 2;
             } else{
-                grades --;
+            	grades += 1;
             }
         }
-    } // while
+        return grades; //60,65,70,75
+     }
+    // } // while
     
-    return grades; //60,65,70,75
+    
 }
 
-solve(67);
+console.log(solve(67));
